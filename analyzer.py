f2a44656b795adfccafcac79c92defeb77ccce5a
ShahidBeldar <shahidbeldar932@gmail.com>
2025-08-26 10:42:43 +0530

    Update analyzer.py
    Imported model Directly form utlis.py

--- analyzer.py
+++ analyzer.py
@@ -1,10 +1,12 @@
 from transformers import pipeline
-from utils import load_data, compute_similarity, compute_sentiment
+from utils import load_data, compute_similarity, compute_sentiment, sentiment_model
 
-# Load HuggingFace sentiment model (DistilBERT fine-tuned on sentiment)
-sentiment_model = pipeline("sentiment-analysis")
+
+
+
 
 def analyze_headline(headline):
+
     # Run sentiment analysis
     result = sentiment_model(headline)[0]  # returns dict: {'label': 'POSITIVE', 'score': 0.98}
     label = result['label']
@@ -14,15 +16,18 @@ def analyze_headline(headline):
     if label == "POSITIVE":
         impact = f" Positive ({score:.2f} confidence) - Price likely to go up"
         polarity = score
+
     else:
         impact = f" Negative ({score:.2f} confidence) - Price likely to go down"
         polarity = -score
 
+
+
     # Load dataset and compute similarity with historical headlines
+
     news_df = load_data("news.csv")
     news_df = compute_sentiment(news_df)  # now also transformer-based
     matched = compute_similarity(news_df, headline)
-
     return {
         'polarity': polarity,
         'impact': impact,
